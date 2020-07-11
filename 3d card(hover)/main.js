cb7afa03deadccf3d8fa233e39e4f7cf46e73c3d
Lavrentev Stanislav <hvoinii@gmail.com>
2020-07-11 12:46:45 +0600

    change querySelectors to event.currentTarget

--- 3d card(hover)/main.js
+++ 3d card(hover)/main.js
@@ -6,12 +6,12 @@ cards.forEach(function(item){
 
 
 function startRotate (event) {
-      const cardItem = this.querySelector('.card-item');
+      const cardItem = event.currentTarget.children[0];
       const halfHeight = cardItem.offsetHeight / 2;
       const halfWidth = cardItem.offsetWidth / 2;
       cardItem.style.transform = `rotateX(${-(event.offsetY - halfHeight)/10 }deg) rotateY(${(event.offsetX - halfWidth)/5 }deg)`;
 }
 function stopRotate(event){
-    const cardItem = this.querySelector('.card-item');
+    const cardItem = event.currentTarget.children[0];
     cardItem.style.transform = "rotate(0)";
 }
