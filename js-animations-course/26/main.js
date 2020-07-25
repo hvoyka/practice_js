3fd28f5f6d2d63938cad5f5dd76de490787cecd7
Lavrentev Stanislav <hvoinii@gmail.com>
2020-07-25 19:04:37 +0600

    add easing - two types

---
+++ js-animations-course/26/main.js
@@ -0,0 +1,14 @@
+const rotatingSquare = document.getElementById('web-animation');
+
+rotatingSquare.animate(
+  [
+    { backgroundColor: "#000000", opacity: 1.0, transform: 'rotate(0deg) translate3d(0,0,0)'/* , easing: 'ease-out' */ },
+    { backgroundColor: "#440000", opacity: 0.3},
+    { backgroundColor: "#000000", opacity: 1.0, transform: 'rotate(360deg) translate3d(0,0,0)'/* , easing: 'ease-in' */}
+  ],
+  {
+    duration: 500,
+    iterations: Infinity,
+    easing: 'ease-in-out'
+  }
+);
