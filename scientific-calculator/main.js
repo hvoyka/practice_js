bc64b99900c9dadf942a23fc46a792363681b257
Lavrentev Stanislav <hvoinii@gmail.com>
2020-07-31 11:31:29 +0600

    add factorial and gamma functions

--- scientific-calculator/main.js
+++ scientific-calculator/main.js
@@ -1,4 +1,4 @@
-let calculator_buttons = [
+/* let calculator_buttons = [
   {
     name: "rad",
     symbol: "Rad",
@@ -208,7 +208,7 @@ let calculator_buttons = [
     formula: "+",
     type: "operator"
   }
-];
+]; */
 /* 
 const input_element = document.querySelector('.input')
 
@@ -228,3 +228,25 @@ input_element.addEventListener('click', event => {
       if(button.name == target_btn.id) calculator(button);
   });
 }); */
+
+function factorial(n) {
+  return n ? n * factorial(n - 1) : 1;
+}
+// GAMMA FUNCTINON
+function gamma(n) {  // accurate to about 15 decimal places
+  //some magic constants 
+  var g = 7, // g represents the precision desired, p is the values of p[i] to plug into Lanczos' formula
+      p = [0.99999999999980993, 676.5203681218851, -1259.1392167224028, 771.32342877765313, -176.61502916214059, 12.507343278686905, -0.13857109526572012, 9.9843695780195716e-6, 1.5056327351493116e-7];
+  if(n < 0.5) {
+    return Math.PI / Math.sin(n * Math.PI) / gamma(1 - n);
+  }
+  else {
+    n--;
+    var x = p[0];
+    for(var i = 1; i < g + 2; i++) {
+      x += p[i] / (n + i);
+    }
+    var t = n + g + 0.5;
+    return Math.sqrt(2 * Math.PI) * Math.pow(t, (n + 0.5)) * Math.exp(-t) * x;
+  }
+}
